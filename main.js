022dd30e42b8d3e9c79b0d7732d043933e493751
nakata04 <nakata951104@yahoo.co.jp>
2022-08-21 13:48:04 +0900

    update 20220821g

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 function header(rootDir){
-    // print("aaaaaaa");
-    // alert(rootDir + "include/header.html");
-    // document.write(rootDir + "include/header.html");
-    // document.write(html);
     $.ajax({
         url: rootDir + "include/header.html",  // 読み込むHTMLファイル
-        // url: "include/header.html",  // 読み込むHTMLファイル
         cache: false,
         async: false,
         dataType: 'html',
         success: function(html){
             html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
             document.write(html);
-            // alert(rootDir + "include/header.html");
         },
         error: function(err){
-            alert(err);
-            // alert("error");
+            alert("error" + err);
         }
     });
 }
+
+
+// Jquery関連のエラーが出たとき下記をhtml上に記載する（head）。
+// ajax通信に問題がないか確認ができる。
+/* 
+<script type="text/javascript">
+    $.ajax({
+        url: "../include/header.html",
+        success: function(data) {
+            alert('success!!');
+        },
+        error: function(err){
+            //通信失敗時の処
+            alert('通信失敗' + err);
+        }
+    });
+</script>
+*/
