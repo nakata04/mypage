19ab32cb3ce328cc90a97039fd792d222ea68cc1
nakata04 <nakata951104@yahoo.co.jp>
2022-08-21 12:43:31 +0900

    update 20220821b

---
+++ main.js
@@ -0,0 +1,22 @@
+function header(rootDir){
+    // print("aaaaaaa");
+    // alert(rootDir + "include/header.html");
+    document.write(rootDir + "include/header.html");
+    // document.write(html);
+    $.ajax({
+        url: rootDir + "include/header.html",  // 読み込むHTMLファイル
+        // url: "include/header.html",  // 読み込むHTMLファイル
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+            document.write(html);
+            alert(rootDir + "include/header.html");
+        },
+        error: function(err){
+            alert(err);
+            // alert("error");
+        }
+    });
+}
